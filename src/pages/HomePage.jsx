e86647049e74c3287be096fa97955c6663410d23
Muhammad Prasetyo <prasetyo.rwi>
2022-08-10 17:03:58 +0700

    update home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
 import React, { Component, Fragment } from "react";
+import Categories from "../components/home/Categories";
+import FeaturedProducts from "../components/home/FeaturedProducts";
+import Collection from "../components/home/Collection";
+import NewArrival from "../components/home/NewArrival";
+import HomeTop from "../components/home/HomeTop";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <h1>This is our Home Page</h1>
+        <HomeTop />
+        <NewArrival />
+        <FeaturedProducts />
+        <Collection />
+        <Categories />
       </Fragment>
     );
   }
